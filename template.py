c431ccb3ed9d477fd801556852bc19eb6e728512
Maciej Markowski <maciej.markowski@hitachienergy.com>
2022-12-04 10:16:05 +0100

    completed day4 tasks

---
+++ template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+"""
+
+"""
+
+
+def taskOne() -> None:
+  print("Nothing yet")
+
+"""
+
+"""
+
+def taskTwo() -> None:
+  print('Nothing yet')
+
+
+
+if __name__ == '__main__':
+  taskOne()
+  taskTwo()
