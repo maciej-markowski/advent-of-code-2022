3bfff60d321b2c4d0d1f11bc21fd04b10c128a58
maciej-markowski <maciekmm@o2.pl>
2022-12-11 12:22:53 +0100

    completed day11 tasks

--- 10/solution.py
+++ 10/solution.py
@@ -40,7 +40,6 @@ Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th c
 """
 
 
-
 def findKey(keyList, target):
   if target in keyList:
     return target
@@ -49,6 +48,7 @@ def findKey(keyList, target):
     for i in keyList:
       if i < target:
         return i
+
 
 def taskOne() -> None:
   aiCyclesToFind = [20, 60, 100, 140, 180, 220]
@@ -77,7 +77,48 @@ def taskOne() -> None:
 
 
 """
+--- Part Two ---
+It seems like the X register controls the horizontal position of a sprite. Specifically, the sprite is 3 pixels wide,
+and the X register sets the horizontal position of the middle of that sprite. (In this system, there is no such thing
+as "vertical position": if the sprite's horizontal position puts its pixels where the CRT is currently drawing,
+then those pixels will be drawn.)
+You count the pixels on the CRT: 40 wide and 6 high. This CRT screen draws the top row of pixels left-to-right, then the
+row below that, and so on. The left-most pixel in each row is in position 0, and the right-most pixel in each row is in position 39.
+Like the CPU, the CRT is tied closely to the clock circuit: the CRT draws a single pixel during each cycle. Representing each pixel
+of the screen as a #, here are the cycles during which the first and last pixel in each row are drawn:
+  Cycle   1 -> ######################################## <- Cycle  40
+  Cycle  41 -> ######################################## <- Cycle  80
+  Cycle  81 -> ######################################## <- Cycle 120
+  Cycle 121 -> ######################################## <- Cycle 160
+  Cycle 161 -> ######################################## <- Cycle 200
+  Cycle 201 -> ######################################## <- Cycle 240
+So, by carefully timing the CPU instructions and the CRT drawing operations, you should be able to determine whether the sprite
+is visible the instant each pixel is drawn. If the sprite is positioned such that one of its three pixels is the pixel currently being drawn,
+the screen produces a lit pixel (#); otherwise, the screen leaves the pixel dark (.).
+The first few pixels from the larger example above are drawn as follows:
+  Sprite position: ###.....................................
+  Start cycle   1: begin executing addx 15
+  During cycle  1: CRT draws pixel in position 0
+  Current CRT row: #
 
+  During cycle  2: CRT draws pixel in position 1
+  Current CRT row: ##
+  End of cycle  2: finish executing addx 15 (Register X is now 16)
+  Sprite position: ...............###......................
+
+  Start cycle   3: begin executing addx -11
+  During cycle  3: CRT draws pixel in position 2
+  Current CRT row: ##.
+
+  (...)
+Allowing the program to run to completion causes the CRT to produce the following image:
+  ##..##..##..##..##..##..##..##..##..##..
+  ###...###...###...###...###...###...###.
+  ####....####....####....####....####....
+  #####.....#####.....#####.....#####.....
+  ######......######......######......####
+  #######.......#######.......#######.....
+Render the image given by your program. What eight capital letters appear on your CRT?
 """
 
 
@@ -89,6 +130,7 @@ def drawPixel(cycle, x) -> str:
   if cycle % 40 == 0:
     c += '\n'
   return c
+
 
 def taskTwo() -> None:
   iCycle = 1
@@ -108,12 +150,7 @@ def taskTwo() -> None:
         iCycle +=1
         iX += int(sInstruction.split()[1])
 
-
-
   print(sOutput)
-
-
-
 
 
 if __name__ == '__main__':
---
+++ 11/solution.py
@@ -0,0 +1,199 @@
+#!/usr/bin/python
+
+"""
+--- Day 11: Monkey in the Middle ---
+As you finally start making your way upriver, you realize your pack is much lighter than you remember.
+Just then, one of the items from your pack goes flying overhead. Monkeys are playing Keep Away with your missing things!
+To get your stuff back, you need to be able to predict where the monkeys will throw your items. After some careful observation,
+you realize the monkeys operate based on how worried you are about each item.
+You take some notes (your puzzle input) on the items each monkey currently has, how worried you are about those items,
+and how the monkey makes decisions based on your worry level. For example:
+  <input-test>
+Each monkey has several attributes:
+  Starting items lists your worry level for each item the monkey is currently holding in the order they will be inspected.
+  Operation shows how your worry level changes as that monkey inspects an item. (An operation like new = old * 5 means 
+  that your worry level after the monkey inspected the item is five times whatever your worry level was before inspection.)
+  Test shows how the monkey uses your worry level to decide where to throw an item next.
+  If true shows what happens with an item if the Test was true.
+  If false shows what happens with an item if the Test was false.
+After each monkey inspects an item but before it tests your worry level, your relief that the monkey's inspection didn't damage the item
+causes your worry level to be divided by three and rounded down to the nearest integer.
+The monkeys take turns inspecting and throwing items. On a single monkey's turn, it inspects and throws all of the items it is holding
+one at a time and in the order listed. Monkey 0 goes first, then monkey 1, and so on until each monkey has had one turn. The process
+of each monkey taking a single turn is called a round.
+When a monkey throws an item to another monkey, the item goes on the end of the recipient monkey's list. A monkey that starts a round
+with no items could end up inspecting and throwing many items by the time its turn comes around. If a monkey is holding no items
+at the start of its turn, its turn ends.
+In the above example, the first round proceeds as follows:
+  Monkey 0:
+    Monkey inspects an item with a worry level of 79.
+      Worry level is multiplied by 19 to 1501.
+      Monkey gets bored with item. Worry level is divided by 3 to 500.
+      Current worry level is not divisible by 23.
+      Item with worry level 500 is thrown to monkey 3.
+    Monkey inspects an item with a worry level of 98.
+      Worry level is multiplied by 19 to 1862.
+      Monkey gets bored with item. Worry level is divided by 3 to 620.
+      Current worry level is not divisible by 23.
+      Item with worry level 620 is thrown to monkey 3.
+(...)
+  Monkey 0: 20, 23, 27, 26
+  Monkey 1: 2080, 25, 167, 207, 401, 1046
+  Monkey 2: 
+  Monkey 3: 
+Monkeys 2 and 3 aren't holding any items at the end of the round; they both inspected items during the round and threw them all before the round ended.
+This process continues for a few more rounds:
+After round 2, the monkeys are holding items with these worry levels:
+  Monkey 0: 695, 10, 71, 135, 350
+  Monkey 1: 43, 49, 58, 55, 362
+  Monkey 2: 
+  Monkey 3: 
+(...)
+After round 20, the monkeys are holding items with these worry levels:
+  Monkey 0: 10, 12, 14, 26, 34
+  Monkey 1: 245, 93, 53, 199, 115
+  Monkey 2: 
+  Monkey 3: 
+Chasing all of the monkeys at once is impossible; you're going to have to focus on the two most active monkeys if you
+want any hope of getting your stuff back. Count the total number of times each monkey inspects items over 20 rounds:
+  Monkey 0 inspected items 101 times.
+  Monkey 1 inspected items 95 times.
+  Monkey 2 inspected items 7 times.
+  Monkey 3 inspected items 105 times.
+In this example, the two most active monkeys inspected items 101 and 105 times. The level of monkey business in this
+situation can be found by multiplying these together: 10605.
+Figure out which monkeys to chase by counting how many items they inspect over 20 rounds. 
+What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?
+"""
+
+
+from tkinter import W
+
+
+def taskOne() -> None:
+  oMonkeys = {}
+  with open('input', 'r') as fInputFile:
+    while True:
+      sLine = fInputFile.readline()
+      if not sLine:
+        break
+      if sLine.startswith('Monkey'):
+        _mID = int(sLine.strip()[-2])
+        sLine = fInputFile.readline()
+        _itm = []
+        for i in sLine.strip().split(':')[1].split(','):
+          _itm.append(int(i))
+        sLine = fInputFile.readline()
+        _op = sLine.strip().split('=')[1].strip()
+        sLine = fInputFile.readline()
+        _divisible_by = int(sLine.strip().split()[-1])
+        sLine = fInputFile.readline()
+        _if_true = int(sLine.strip().split()[-1])
+        sLine = fInputFile.readline()
+        _if_false = int(sLine.strip().split()[-1])
+        oMonkeys[_mID] = {
+                          'items': _itm,
+                          'operation': _op,
+                          'test': _divisible_by,
+                          'if_true': _if_true,
+                          'if_false': _if_false,
+                          'inspected': 0
+                        }
+  
+  iRounds = 20
+
+  for _ in range(iRounds):
+    for i in range(len(oMonkeys)):
+      for item in oMonkeys[i]['items'].copy():
+        if oMonkeys[i]['operation'].split()[1] == '+':
+          worry_level = item + int(oMonkeys[i]['operation'].split()[2])
+        elif oMonkeys[i]['operation'].split()[2] == 'old':
+          worry_level = item * item
+        else:
+          worry_level = item * int(oMonkeys[i]['operation'].split()[2])
+        worry_level = worry_level // 3
+        if worry_level %  oMonkeys[i]['test'] == 0:
+          oMonkeys[oMonkeys[i]['if_true']]['items'].append(worry_level)
+          oMonkeys[i]['items'].pop(0)
+        else:
+          oMonkeys[oMonkeys[i]['if_false']]['items'].append(worry_level)
+          oMonkeys[i]['items'].pop(0)
+        oMonkeys[i]['inspected'] += 1
+
+  iInspections = []
+  for i in range(len(oMonkeys)):
+    iInspections.append(oMonkeys[i]['inspected'])
+  iInspections.sort()
+
+  print("Monkey business is {}".format(str(iInspections[-1] * iInspections[-2])))
+ 
+
+"""
+
+"""
+
+def taskTwo() -> None:
+  oMonkeys = {}
+  with open('input', 'r') as fInputFile:
+    while True:
+      sLine = fInputFile.readline()
+      if not sLine:
+        break
+      if sLine.startswith('Monkey'):
+        _mID = int(sLine.strip()[-2])
+        sLine = fInputFile.readline()
+        _itm = []
+        for i in sLine.strip().split(':')[1].split(','):
+          _itm.append(int(i))
+        sLine = fInputFile.readline()
+        _op = sLine.strip().split('=')[1].strip()
+        sLine = fInputFile.readline()
+        _divisible_by = int(sLine.strip().split()[-1])
+        sLine = fInputFile.readline()
+        _if_true = int(sLine.strip().split()[-1])
+        sLine = fInputFile.readline()
+        _if_false = int(sLine.strip().split()[-1])
+        oMonkeys[_mID] = {
+                          'items': _itm,
+                          'operation': _op,
+                          'test': _divisible_by,
+                          'if_true': _if_true,
+                          'if_false': _if_false,
+                          'inspected': 0
+                        }
+  
+  iRounds = 10000
+  MOD = 1
+  for i in range(len(oMonkeys)):
+    MOD *= oMonkeys[i]['test']
+
+  for _ in range(iRounds):
+    for i in range(len(oMonkeys)):
+      for item in oMonkeys[i]['items'].copy():
+        if oMonkeys[i]['operation'].split()[1] == '+':
+          worry_level = item + int(oMonkeys[i]['operation'].split()[2])
+        elif oMonkeys[i]['operation'].split()[2] == 'old':
+          worry_level = item * item
+        else:
+          worry_level = item * int(oMonkeys[i]['operation'].split()[2])
+        worry_level = worry_level % MOD
+        if worry_level %  oMonkeys[i]['test'] == 0:
+          oMonkeys[oMonkeys[i]['if_true']]['items'].append(worry_level)
+          oMonkeys[i]['items'].pop(0)
+        else:
+          oMonkeys[oMonkeys[i]['if_false']]['items'].append(worry_level)
+          oMonkeys[i]['items'].pop(0)
+        oMonkeys[i]['inspected'] += 1
+
+  iInspections = []
+  for i in range(len(oMonkeys)):
+    iInspections.append(oMonkeys[i]['inspected'])
+  iInspections.sort()
+
+  print("[worry level self decreased] Monkey business is {}".format(str(iInspections[-1] * iInspections[-2])))
+
+
+
+if __name__ == '__main__':
+  taskOne()
+  taskTwo()
